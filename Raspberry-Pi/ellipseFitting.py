2fd062940aed5faa6a774bd9a10562e601b64c61
Ankur Raj <30729895+ankurrajw@users.noreply.github.com>
2022-04-08 14:25:33 +0200

    Added logger and colour image save

--- Raspberry-Pi/ellipseFitting.py
+++ Raspberry-Pi/ellipseFitting.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import time
 import os
 import random as rng
+import logging
 
 
 srcPiCam = 'libcamerasrc ! video/x-raw,width=640,height=480,framerate=90/1 ! videoflip method=clockwise ! videoconvert ! appsink'
@@ -13,6 +14,18 @@ cap = cv.VideoCapture(srcPiCam)
 CANNY_THRESHOLD = 100
 MEDIAN_BLUR_K_SIZE = 10
 MORPH_K_SIZE = 1
+
+'''logger setup'''
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.FileHandler("../log_ellipse_fitting.log")
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+logger.info("*" * 50)
+logger.info("EXPERIMENT START")
 
 
 def create_workspace():
@@ -25,16 +38,17 @@ def create_workspace():
 
     return name_workspace
 
-def filter_contour(contours):
+
+def filter_contour(_contours):
     """TODO filter contours to get ellipses based on area and circularity
     DOCUMENTATION : Why we need to do a convex hull operation on the contour instead of finding the circularity directly
     from contour ?
     ANS: Since pixels of contour leads to a higher th_value of circularity > 200. Doing a convex hull leads to a lower
     th_value since we don't deal with discritised pixels """
-    contours_filtered = []
-    print("Initial Contours : {}".format(len(contours)))
+    _contours_filtered = []
+    print("Initial Contours : {}".format(len(_contours)))
     print("Filtered Contours:")
-    for i, c in enumerate(contours):
+    for i, c in enumerate(_contours):
         try:
             convex_hull = cv.convexHull(c)
             area_hull = cv.contourArea(convex_hull)
@@ -44,32 +58,46 @@ def filter_contour(contours):
                 circularity_hull = (4 * np.pi * area_hull) / circumference_hull ** 2
                 if 0.8 < circularity_hull:  # filtering based on circularity
                     print("convex hull :{} Circularity :{} Area : {}".format(i, circularity_hull, area_hull))
-                    contours_filtered.append(convex_hull)
+                    _contours_filtered.append(convex_hull)
         except ZeroDivisionError:
             print("Division by zero for contour {}".format(i))
-    return contours_filtered
+    return _contours_filtered
 
 
-def draw_ellipse(drawing, contours_filtered):
-    minEllipse = [None] * len(contours_filtered)
-    for i, c in enumerate(contours_filtered):
+def draw_ellipse(_drawing, _contours_filtered):
+    minEllipse = [None] * len(_contours_filtered)
+    for i, c in enumerate(_contours_filtered):
         color = (rng.randint(0, 256), rng.randint(0, 256), rng.randint(0, 256))
         minEllipse[i] = cv.fitEllipse(c)
-        cv.drawContours(drawing, contours_filtered, i, color)
+        cv.drawContours(_drawing, _contours_filtered, i, color)
         (x, y), (MA, ma), angle = minEllipse[i]
         area_contour_hull = cv.contourArea(c)
         area_ellipse = (np.pi / 4) * MA * ma
         print("Area Ellipse :{} Area Contour Hull :{}".format(area_ellipse, area_contour_hull))
-        cv.ellipse(drawing, minEllipse[i], color, 2)
+        cv.ellipse(_drawing, minEllipse[i], color=color, thickness=2)
+    return _drawing
+
+
+def draw_ellipse_rgb(_image, _contours):
+    minEllipse = [None] * len(_contours)
+    for i, c in enumerate(_contours):
+        minEllipse[i] = cv.fitEllipse(c)
+        color = (rng.randint(0, 256), rng.randint(0, 256), rng.randint(0, 256))
+        cv.drawContours(_image, _contours, i, color)
+        cv.ellipse(_image, minEllipse[i], color=color, thickness=2)
+    return _image
 
 
 count = 0
-folder_name = create_workspace()
-
+ellipse_detected = 0
+multiple_ellipses = 0
 fps_start_time = 0
 fps = 0
+total_images = 600
+folder_name = create_workspace()
 
-while count < 600:
+while count < total_images:
+    logger.info(f"Values for Canny -{CANNY_THRESHOLD} Blur K size -{MEDIAN_BLUR_K_SIZE} Morph -{MORPH_K_SIZE}")
     ret, frame = cap.read()
     fps_end_time = time.time()
     time_diff = fps_end_time - fps_start_time
@@ -80,12 +108,9 @@ while count < 600:
         roi = frame[220:640, 0:480]
         output = roi.copy()
         src_gray = cv.cvtColor(roi, cv.COLOR_BGR2GRAY)
-        #cl1 = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-        #clahe = cl1.apply(src_gray)
 
         src_gray = cv.medianBlur(src_gray, MEDIAN_BLUR_K_SIZE)
         kernel = np.ones((MORPH_K_SIZE, MORPH_K_SIZE), np.uint8)
-        # _, src_gray = cv.threshold(src_gray, 100, 255, cv.THRESH_BINARY_INV)
         opening = cv.morphologyEx(src_gray, cv.MORPH_OPEN, kernel)
 
         canny_output = cv.Canny(opening, CANNY_THRESHOLD, CANNY_THRESHOLD * 2)
@@ -93,32 +118,24 @@ while count < 600:
         contours_filtered = filter_contour(contours)
 
         drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
-        draw_ellipse(drawing, contours_filtered)
-
+        drawing = draw_ellipse(drawing, contours_filtered)
         cv.imwrite(folder_name + "ellipse" + str(count) + ".png", drawing)
-'''
-        if contours is not None:
-            count += 1
-            minEllipse = [None] * len(contours)
-            """TODO: Why the for loop is done twice?"""
-            for i, c in enumerate(contours):
-                if 50 < c.shape[0] < 150:
-                    minEllipse[i] = cv.fitEllipse(c)
-                    print(minEllipse[i])
-            # drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
 
-            for i, c in enumerate(contours):
-                color = (rng.randint(0, 256), rng.randint(0, 256), rng.randint(0, 256))
-                # contour
-                # cv.drawContours(drawing, contours, i, color)
-                # ellipse
-                if 50 < c.shape[0] < 150:
-                    cv.ellipse(roi, minEllipse[i], color=color, thickness=2)
-                    cv.imwrite(folder_name + "ellipse" + str(count) + ".png", roi)
+        '''Comment if colour images are not needed'''
+        colour_image = draw_ellipse_rgb(roi, contours_filtered)
+        cv.imwrite(folder_name + "colour" + str(count) + ".png", colour_image)
 
-        else:
-            count += 1
-            cv.imwrite(folder_name + "ellipse" + str(count) + ".png", frame)
-'''
+        ellipse_detected += len(contours_filtered)
+        if len(contours_filtered) > 1:
+            multiple_ellipses += len(contours_filtered) - 1
+
+        count += 1
 
 cap.release()
+cv.destroyAllWindows()
+
+logger.info("Ellipse Single Detected {}, Multiple Ellipse {}, Total Images {}, Percentage Detection {}".format(
+    (ellipse_detected - multiple_ellipses), multiple_ellipses, total_images,
+    (ellipse_detected - multiple_ellipses) / total_images))
+logger.info("EXPERIMENT END")
+logger.info("*" * 50)
