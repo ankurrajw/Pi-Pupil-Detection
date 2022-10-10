687404e9031781f9bbe244d3fb7a40a841bab5b7
Ankur Raj <30729895+ankurrajw@users.noreply.github.com>
2022-10-10 10:22:59 +0200

    added feature to look at all files in the folder

--- Windows/report-thesis/detection_comparison.py
+++ Windows/report-thesis/detection_comparison.py
@@ -1,29 +1,31 @@
+import glob
+
 import cv2 as cv
 import numpy as np
 import random as rng
 import pandas as pd
+from pathlib import Path
 
-cap = cv.VideoCapture(
-    r"C:\Users\Ankur\Desktop\Uni Siegen\SEM5\Eye Detection\Project-code-Ankur\master-thesis-eye-tracking\Windows\report-thesis\1\9.avi")
-
-CANNY_THRESHOLD = 40
+#folder_path = r'C:\Users\Ankur\Downloads\LPW'
+# 25 canny original 23 blur
+CANNY_THRESHOLD = 10
 MEDIAN_BLUR_K_SIZE = 23
 MORPH_K_SIZE = 1
 
-column = ('x', 'y', 'extra')
-df_pupil_location = pd.DataFrame(columns=column)
+folder_path = r"C:\Users\Ankur\Desktop\Uni Siegen\SEM5\Eye Detection\Project-code-Ankur\master-thesis-eye-tracking\Windows\report-thesis\evaluation\without_glasses\7"
+file_path = glob.glob(folder_path+r"\*avi")
 
 
 def filter_contour(_contours):
     _contours_filtered = []
-    print("Initial Contours : {}".format(len(_contours)))
-    print("Filtered Contours:")
+    #print("Initial Contours : {}".format(len(_contours)))
+    #print("Filtered Contours:")
     for i, c in enumerate(_contours):
         try:
             convex_hull = cv.convexHull(c)
             area_hull = cv.contourArea(convex_hull)
             # print("{} area convex hull {}".format(i, area_hull))
-            if 2000 < area_hull:  # filtering based on area
+            if 2000 < area_hull < 30000:  # filtering based on area
                 circumference_hull = cv.arcLength(convex_hull, True)
                 circularity_hull = (4 * np.pi * area_hull) / circumference_hull ** 2
                 if 0.8 < circularity_hull:  # filtering based on circularity
@@ -43,7 +45,7 @@ def draw_ellipse(_drawing, _contours_filtered):
         (x, y), (MA, ma), angle = minEllipse[i]
         area_contour_hull = cv.contourArea(c)
         area_ellipse = (np.pi / 4) * MA * ma
-        print("Area Ellipse :{} Area Contour Hull :{}".format(area_ellipse, area_contour_hull))
+        # print("Area Ellipse :{} Area Contour Hull :{}".format(area_ellipse, area_contour_hull))
         cv.ellipse(_drawing, minEllipse[i], color=color, thickness=2)
     return _drawing
 
@@ -60,49 +62,54 @@ def calculate_centre(_contours_filtered):
     return _location
 
 
-while cap.isOpened():
-    ret, frame = cap.read()
+for file in file_path:
+    cap = cv.VideoCapture(file)
+    print(file)
+    column = ('x', 'y', 'extra')
+    df_pupil_location = pd.DataFrame(columns=column)
+    while cap.isOpened():
+        ret, frame = cap.read()
 
-    if ret:
-        src_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        src_gray = cv.medianBlur(src_gray, MEDIAN_BLUR_K_SIZE)
-        kernel = np.ones((MORPH_K_SIZE, MORPH_K_SIZE), np.uint8)
-        opening = cv.morphologyEx(src_gray, cv.MORPH_OPEN, kernel)
+        if ret:
+            src_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            src_gray = cv.medianBlur(src_gray, MEDIAN_BLUR_K_SIZE)
+            kernel = np.ones((MORPH_K_SIZE, MORPH_K_SIZE), np.uint8)
+            opening = cv.morphologyEx(src_gray, cv.MORPH_OPEN, kernel)
 
-        canny_output = cv.Canny(opening, CANNY_THRESHOLD, CANNY_THRESHOLD * 2)
-        contours, _ = cv.findContours(canny_output, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-        contours_filtered = filter_contour(contours)
+            canny_output = cv.Canny(opening, CANNY_THRESHOLD, CANNY_THRESHOLD * 2)
+            contours, _ = cv.findContours(canny_output, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+            contours_filtered = filter_contour(contours)
 
-        drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
-        drawing = draw_ellipse(drawing, contours_filtered)
+            drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
+            drawing = draw_ellipse(drawing, contours_filtered)
 
-        location = calculate_centre(contours_filtered)
-        if not location:
-            data_entry = pd.DataFrame.from_dict({
-                'x': [[]],
-                'y': [[]],
-                'extra': [[]]})
-            df_pupil_location = pd.concat([df_pupil_location, data_entry], ignore_index=True)
-            print("empty array")
-        else:
-            for i in range(len(location)):
+            location = calculate_centre(contours_filtered)
+            if not location:
                 data_entry = pd.DataFrame.from_dict({
-                    'x': [location[i][0]],
-                    'y': [location[i][1]],
+                    'x': [0],
+                    'y': [0],
+                    'extra': [0]})
+                df_pupil_location = pd.concat([df_pupil_location, data_entry], ignore_index=True)
+                # print("empty array")
+            else:
+                data_entry = pd.DataFrame.from_dict({
+                    'x': [location[0][0]],
+                    'y': [location[0][1]],
                     'extra': [location[1:]]})
 
-                print(f"location - {data_entry}")
+                #print(f"location - {data_entry}")
                 df_pupil_location = pd.concat([df_pupil_location, data_entry], ignore_index=True)
 
-        cv.imshow("output", frame)
-        cv.imshow("canny", canny_output)
-        cv.imshow("pupil", drawing)
-        key = cv.waitKey(1)
-        if key == 27:
+            cv.imshow("output", frame)
+            cv.imshow("canny", canny_output)
+            cv.imshow("pupil", drawing)
+            key = cv.waitKey(10000)
+            if key == 27:
+                break
+        else:
             break
-    else:
-        break
-cap.release()
-cv.destroyAllWindows()
+    file_name = Path(file).stem
+    # df_pupil_location.to_csv(folder_path + rf"\Experiment_{int(file_name)}_canny_{CANNY_THRESHOLD}_blur_{MEDIAN_BLUR_K_SIZE}.csv", sep='\t', index=False)
+    cap.release()
 
-df_pupil_location.to_csv(f"Experiment.csv", sep='\t', index=False)
+cv.destroyAllWindows()
